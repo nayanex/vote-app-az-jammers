6b72b1fbcd48022408c3b377077e680fe16990a7
Nayana Holanda de Abreu <holanda.nayana@gmail.com>
2021-09-12 15:32:22 +0200

    FIX - debug mode ON

--- azure-vote/main.py
+++ azure-vote/main.py
@@ -198,7 +198,7 @@ def index():
 
 if __name__ == "__main__":
     # comment line below when deploying to VMSS
-    app.run()  # local
+    #app.run()  # local
     # uncomment the line below before deployment to VMSS
     #app.run(host="0.0.0.0", threaded=True, debug=True)  # remote
-    # app.run(host='0.0.0.0', threaded=True, debug=True, port=5000) # remote
+    app.run(host='0.0.0.0', threaded=True, debug=True, port=5000) # remote
